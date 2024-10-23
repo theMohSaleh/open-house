109b152d527562c57056c1876057a4e8902729e1
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-23 11:31:11 +0300

    built landing page for listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,4 +3,14 @@ const router = express.Router();
 
 const Listing = require('../models/listing');
 
+router.get('/', async (req, res) => {
+    try {
+        const listings = await Listing.find({});
+        console.log('All listings', listings);
+        res.render('listings/index.ejs');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 module.exports = router;
